a3d13afa8e39a04ace480cdbccc64be582165be9
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-09-21 23:19:15 +0530

    array example

---
+++ 19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x[]={1,2,3,5,6,3,9,7,2,9,8};
+    int n=sizeof(x)/sizeof(x[0]);
+    int temp=x[n-1];
+    for(int i=n-1;i>0;i--) x[i]=x[i-1];
+    x[0]=temp;
+    for(int i=0;i<n;i++) cout << x[i] << " ";
+    return 0;
+}
