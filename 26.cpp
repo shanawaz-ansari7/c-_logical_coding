60fe5471e88b0c4f06834b3fd824f88bdc99af96
shanawaz-ansari7 <shanawaz.ansari9380@gmail.com>
2026-09-22 22:57:00 +0530

    array example

---
+++ 26.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x[]={1,2,3,5,61,3,19,7,22,9,80};
+    int n=sizeof(x)/sizeof(x[0]), count=0;
+    for(int i=0;i<n-count;i++) {
+        if(x[i]<10) {
+            for(int j=i;j<n-count-1;j++) x[j]=x[j+1];
+            i--; count++;
+        }
+    }
+    for(int i=0;i<n-count;i++) cout << x[i] << " ";
+    return 0;
+}
